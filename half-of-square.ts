f56723d9bc0ecb853daf961bbe0aad0915f8a57c
royeradames <royeraadames@gmail.com>
2021-12-02 11:42:58 -0500

    📦complete problem

--- half-of-square.ts
+++ half-of-square.ts
@@ -1,1 +1,28 @@
 console.log("half-of-square.ts");
+/* Write a program that uses only two output statements, console.log # and \n to proudce a pattern of hash symbols shaped like half of a perfect 5 x 5 squares (or a right tirangle):
+* For TS use console.log and "#"
+#####
+####
+###
+##
+#
+*/
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("######");
+//   console.log("\n");
+// }
+// console.log("######");
+// console.log("#####");
+// console.log("####");
+// console.log("###");
+// console.log("##");
+// console.log("#");
+
+let size = 5;
+for (let rows = 0; rows < size; rows++) {
+  // size - (rows) = number of #
+  console.log("#".repeat(size - rows));
+}
+
+console.log("-------------------");
