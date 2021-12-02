5e8029a237c3f96dec36c853d5a7c35da76a5ab8
royeradames <royeraadames@gmail.com>
2021-12-02 12:20:40 -0500

    📦complete sideway challange

--- sideways-triangle.ts
+++ sideways-triangle.ts
@@ -11,4 +11,33 @@ console.log("sideways-triangle.ts");
     #
  */
 
+let column = 7;
+let inverRow;
+let countingDown = 2;
+for (let rows = 1; rows <= column; rows++) {
+  // on the 4 iteration start counting down
+  if (rows > 4) {
+    inverRow = rows - countingDown;
+    console.log("#".repeat(inverRow));
+    countingDown += 2;
+  } else console.log("#".repeat(rows));
+}
+
+/* 
+    Notes:
+    #++
+    if 4 iteration then count down
+    if rows > 4
+    then move to closing loop while using the counter has 
+        don't change the counter instead use the counter for math that reduces
+        the displayin pattern
+        rows(5) - 2 = 3 
+        rows(6) - 4 = 2
+        rows(7) - 6 = 1
+        rows - inverRows 
+        inverseRow = rows - countingDown
+        countingDown = 2 (Must take off the increment and remove one )
+        countingDown++
+ */
+
 console.log("--------------------");
