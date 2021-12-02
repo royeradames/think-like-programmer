6e79b6302e724fe1461b881185166b396fd9fbb1
royeradames <royeraadames@gmail.com>
2021-12-02 11:57:37 -0500

    📦innit sideways problem

---
+++ sideways-triangle.ts
@@ -0,0 +1,14 @@
+console.log("sideways-triangle.ts");
+/* 
+    Write a program that uses only two output statements, count << '#' and count << '\n',
+    to produce a pattern of hash sysmbols shaped like a sideways triangle:
+    #
+    ##
+    ###
+    ####
+    ###
+    ##
+    #
+ */
+
+console.log("--------------------");
