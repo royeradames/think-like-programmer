47f659e8de04e87a0c051f95520e8786b01f538b
royeradames <royeraadames@gmail.com>
2021-12-06 14:32:05 -0500

    📦high level structure the solution

--- lunch-checksum-validation.ts
+++ lunch-checksum-validation.ts
@@ -4,9 +4,19 @@ The luhn formula is a widely used system for validating identification numbes.
 Using the original number: 
 - double the value of every other digit. 
 - add the values of the individual digits together 
-- if a double value now has two digits, add the digits individually. 
+    - if a double value now has two digits, add the digits individually. 
 - The identification number is valid if the sum is divisble by 10.
 Write a pgroam that takes an identificaiton number of arbitrary length and process each character before reading the next one.
 */
-
+function lunchFormula(identificationNumbers: string) {
+  function doubleEachDigit() {
+    function isEveryOtherDigit() {}
+  }
+  function addIndividualDigits() {}
+  function identifyNumber() {
+    function isDivisibleBy10() {}
+  }
+}
+const identificationNumbers = "176248";
+lunchFormula(identificationNumbers);
 console.log("-------------------------");
