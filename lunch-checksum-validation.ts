44a218b5954e7012850752b9dc52d2496e0ac67d
royeradames <royeraadames@gmail.com>
2021-12-06 14:29:37 -0500

    📦innit problem

---
+++ lunch-checksum-validation.ts
@@ -0,0 +1,12 @@
+console.log("lunch-checksum-validation");
+/* 
+The luhn formula is a widely used system for validating identification numbes. 
+Using the original number: 
+- double the value of every other digit. 
+- add the values of the individual digits together 
+- if a double value now has two digits, add the digits individually. 
+- The identification number is valid if the sum is divisble by 10.
+Write a pgroam that takes an identificaiton number of arbitrary length and process each character before reading the next one.
+*/
+
+console.log("-------------------------");
