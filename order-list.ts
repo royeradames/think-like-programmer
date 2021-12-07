9b976da085b8fb2f61cedc8210626a7754bec278
royeradames <royeraadames@gmail.com>
2021-12-07 12:21:04 -0500

    📦solve when there is 1 or less in the list

--- order-list.ts
+++ order-list.ts
@@ -1,17 +1,17 @@
 console.log("order-list");
 /* order a given list of numbers */
 
-const listToOder = [
-  [6, 2, 3, 8],
-  [20, 8, 9, 10, 4],
-  [0, 3],
-];
+const listToOder = [[0], [7, 3], [6, 2, 4], [6, 2, 3, 8], [20, 8, 9, 10, 4]];
 listToOder.forEach((list) => {
   orderList(list);
 });
 
 function orderList(list: number[]) {
   let orderList;
+  /* one or less */
+  if (list.length <= 1) return list;
+  /* a pair */
+
   console.log(orderList);
 }
 
