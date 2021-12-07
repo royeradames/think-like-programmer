e8e0a61e4fc20f796b54a353a798c73d8784f58c
royeradames <royeraadames@gmail.com>
2021-12-07 12:16:28 -0500

    📦init problem

---
+++ order-list.ts
@@ -0,0 +1,22 @@
+console.log("order-list");
+/* order a given list of numbers */
+
+const listToOder = [
+  [6, 2, 3, 8],
+  [20, 8, 9, 10, 4],
+  [0, 3],
+];
+listToOder.forEach((list) => {
+  orderList(list);
+});
+
+function orderList(list: number[]) {
+  let orderList;
+  console.log(orderList);
+}
+
+console.log("----------");
+
+/* notes
+    this can be done in place or by making a new list
+*/
