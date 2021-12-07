73373adf8b2642d16f9301cb24e0a7b6830a54b6
royeradames <royeraadames@gmail.com>
2021-12-07 12:38:02 -0500

    📦limit the solution for 2 digit list

--- order-list.ts
+++ order-list.ts
@@ -9,10 +9,26 @@ listToOder.forEach((list) => {
 function orderList(list: number[]) {
   let orderList;
   /* one or less */
-  if (list.length <= 1) return list;
-  /* a pair */
-
-  console.log(orderList);
+  if (list.length <= 1) console.log(list);
+  else if (list.length === 2) {
+    /* a pair */
+    const start = {
+      value: list[0],
+      index: 0,
+    };
+    const end = {
+      value: list[1],
+      index: 1,
+    };
+    const IsSwitchValues = start.value > end.value;
+    if (IsSwitchValues) {
+      list[start.index] = end.value;
+      list[end.index] = start.value;
+      console.log(list);
+    } else console.log(list);
+  } else {
+    /* 3+ */
+  }
 }
 
 console.log("----------");
