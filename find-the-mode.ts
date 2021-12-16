d3912abbf57f10cd64df1b174ade092dfb940ddd
royeradames <royeraadames@gmail.com>
2021-12-16 11:22:02 -0500

    📦init problem

---
+++ find-the-mode.ts
@@ -0,0 +1,13 @@
+/* 
+  In statistics, the mode of a set of values is the value that appears most often. 
+  Write code that processes an array of survey data, where survey takers have responded to a question with a number in the range 1-10, 
+  to determine the mode of the data set. For our purpose, if multiple modes exist, any may be chosen. 
+ */
+console.log("find-the-mode.ts");
+const survey = [4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10];
+function findMode(numberList: number[]) {}
+console.log("------------------------------------");
+
+/* Note:
+  
+ */
